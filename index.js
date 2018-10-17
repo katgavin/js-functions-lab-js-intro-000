ad7f72b20d19b9d5f301fbaf2cd47b612582f5dd
Kat Gavin <marikathryn@gmail.com>
2018-10-17 10:50:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // write your code below!
 function happyHoliday() {
-  return "Happy holidays!"
+  return ("Happy holidays!")
 }
 
 function happyHolidaysTo(name){
-
+  
 }
 
 function happyHolidayTo(name)
 
 function holidayCountdown(holiday, days) {
-
+  
 }
